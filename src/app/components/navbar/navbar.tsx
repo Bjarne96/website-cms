8e69cb61c8a6dd47921c85841c1a1efa3554f1e9
Bjarne96 <bjarne.abb@gmail.com>
2021-02-23 01:52:42 +0100

    navbar transparent fix

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -6,6 +6,7 @@ import { IRouteArray } from './../../interfaces/componentInterfaces'
 
 interface INavbarState {
     loading: boolean;
+    transparent: boolean;
 }
 
 interface INavbarProps {
@@ -21,23 +22,48 @@ export default class Navbar extends React.Component<INavbarProps, INavbarState> 
     constructor(props) {
         super(props);
         this.navbarEvent = this.navbarEvent.bind(this);
+        this.navbarRouteChange = this.navbarRouteChange.bind(this);
+        this.evaluateStyle = this.evaluateStyle.bind(this);
         this.state = {
-            loading: true
+            loading: false,
+            transparent: false
         }
     }
 
     componentDidMount() {
         window.onscroll = this.navbarEvent;
+
         this.setState({ loading: false })
+        console.log('mount');
+        this.evaluateStyle(this.props.history.location.pathname);
     }
 
     navbarEvent() {
         if (window.scrollY > 200) document.getElementById("navbar").classList.add("animateT");
     }
 
+    navbarRouteChange(url) {
+        this.evaluateStyle(url);
+        this.props.changeHistory(url);
+    }
+
+    evaluateStyle(url) {
+        if (url == "/home") {
+            this.setState({ transparent: true })
+        } else {
+            this.setState({ transparent: false })
+        }
+    }
+
     render() {
+        console.log('render');
         if (this.state.loading) return <Loader active />
-        return <nav id="navbar" className="navbar-container misterT" key="mainnav">
+        let navbarClassName = "navbar-container misterT";
+        if (this.state.transparent) {
+            console.log('this.state.transparent', this.state.transparent);
+            navbarClassName += " transparent"
+        }
+        return <nav id="navbar" className={navbarClassName} key="mainnav">
             <img src="./../../../public/logo.png" className="logo" alt="logo" onClick={() => window.location.href = config.domain} />
             <div onClick={() => window.location.href = config.domain}><h2>Tiefschlafen</h2></div>
             <div className={"navbar-menu"}>
@@ -47,7 +73,7 @@ export default class Navbar extends React.Component<INavbarProps, INavbarState> 
                     return <Menu.Item
                         as='div'
                         key={key + "route"}
-                        onClick={() => this.props.changeHistory(route.url)}>
+                        onClick={() => this.navbarRouteChange(route.url)}>
                         <p className={itemClass}>{route.title}</p>
                     </Menu.Item>
                 })}
