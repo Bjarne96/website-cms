24b3d645b107c8782f9ba623b5cfaa388d74b646
Bjarne96 <bjarne.abb@gmail.com>
2021-02-28 21:47:28 +0100

    card style changes + product detail dummy

--- src/app/components/displayArticle/displayDetails/displayDetails.tsx
+++ src/app/components/displayArticle/displayDetails/displayDetails.tsx
@@ -8,6 +8,16 @@ interface IProps {
 
 export class DisplayDetails extends React.Component<IProps, any> {
     render() {
-        return<></>;
+        return <div className="pd-outerframe">
+            <div className="pd-container">
+                <div className="pd-bg-layer">
+                    <div className="pd-title-frame">
+                        <div className="pd-title">Product Name</div>
+                    </div>
+                </div>
+                <div className="pd-front-layer"></div>
+                <div className="dummy-blanket"></div>
+            </div>
+        </div>;
     }
 }
