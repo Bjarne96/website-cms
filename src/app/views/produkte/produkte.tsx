60ae314b2060a022e169a64b8827ebdc96240df7
Bjarne96 <bjarne.abb@gmail.com>
2021-03-18 18:11:41 +0100

    second product changes

--- src/app/views/produkte/produkte.tsx
+++ src/app/views/produkte/produkte.tsx
@@ -13,40 +13,59 @@ export class Produkte extends React.Component<IProps, IState>{
     constructor(props) {
         super(props);
         this.renderProducts = this.renderProducts.bind(this);
-        this.displayPictures = this.displayPictures.bind(this);     
+        this.displayPictures = this.displayPictures.bind(this);
         this.state = {
             loading: true
         }
     }
 
     render() {
-        return <div className="ProdukteContainer"><div className="Produkte">
-            {this.renderProducts(this.props.products)}
-            {this.displayPictures(this.props.products)}
-        </div></div>
+        return <div className="prodcuts-container">
+            <div
+                key={this.props.products[0]._id}
+                className="product"
+            >
+                <img className="product-bild" src={this.props.products[0].variants[0].pictures[0]} alt={this.props.products[0].variants[0].pictures[0]} />
+                <h1>{this.props.products[0].name}</h1><p>{this.props.products[0].variants[0].price}</p>
+            </div>
+            <div
+                key={this.props.products[1]._id}
+                className="product"
+            >
+                <img className="product-bild" src={this.props.products[1].variants[0].pictures[0]} alt={this.props.products[0].variants[0].pictures[0]} />
+                <h1>{this.props.products[1].name}</h1><p>{this.props.products[1].variants[0].price}</p>
+            </div>
+        </div>
     }
 
-    renderProducts(products: Array<IProduct>) {
-        return products.map((Produkts: IProduct) => {
-            return <div 
-                key={Produkts._id}
-            >
-                {Produkts.name}
-                    </div>
+    renderProducts() {
+        return this.props.products.map((Product: IProduct, index) => {
+            console.log('Product', Product);
+            return (<>
+                <div
+                    key={Product._id}
+                    className="product"
+                    id={"test" + { index }}
+                >
+                    <img className="product-bild" src={Product.variants[0].pictures[0]} alt={Product.variants[0].pictures[0]} />
+                    <h1>{Product.name}</h1><p>{Product.variants[0].price}</p>
+                </div>
+            </>
+            )
+
         });
-    }  
-    displayPictures(products: Array<IProduct>) {
-       return   <html><div className= "header"><p>Our wonderful <strong>products</strong></p></div>   
-                        <div className = "Textzug1"><p>1. Variante unseres Produkts</p></div>
-                        <div className = "Textzug2"><p>2. Variante unseres Produkts</p></div>    
-                        <div className="actualPicture1"><img src = {this.props.loadedBackbone.products[0].Variants[0].pictures[0]}  alt = "Error 404, Connection lost"></img> </div>
-                        <div className="actualPicture2"><img src = {process.env.PUBLIC_URL + "/logo.png"}  alt = "Error 404, ConnectionDDDD lost"></img> </div>
-                    // Ending of description
-                    <div className="description">
-                        <p>lalal</p>
-                    </div>
-                <div className="submit area"></div>
-                <input type="submit" value="In den Warenkorb legen"/>
-                </html>
-    } 
+    }
+    displayPictures() {
+        return <>
+            <div className="header"><p>Our wonderful <strong>products</strong></p></div>
+            <div className="Textzug1"><p>1. Variante unseres Produkts</p></div>
+            <div className="actualPicture1"><img src={this.props.products[0].variants[0].pictures[0]} alt="Error 404, Connection lost" /> </div>
+            // Ending of description
+            <div className="description">
+                <p>lalal</p>
+            </div>
+            <div className="submit area"></div>
+            <input type="submit" value="In den Warenkorb legen" />
+        </>
+    }
 }
