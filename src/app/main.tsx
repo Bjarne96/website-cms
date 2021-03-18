3ea8597c2d5453b826850e17f53a9e1f8bc9f5b7
AbbiTheLegend <Peter.abb2000@web.de>
2021-03-18 17:19:04 +0100

    Merged conflict solved

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -85,12 +85,8 @@ export class Main extends React.Component<any, IMainState> {
                         <div className="mainContainer">
                             <Route path="/home" exact component={() => <Home />} />
                             <Route path="/paypal" exact component={() => <Paypal />} />
-<<<<<<< HEAD
                             <Route path="/produkte" exact component={() => <Produkte products={loadedBackbone.products} />} />
-=======
                             <Route path="/success" exact component={() => <Success />} />
-                            <Route path="/produkte" exact component={() => <Produkte />} />
->>>>>>> 62019a1867cfc8056445ad2dd25149b14ccf6410
                             {this.renderArticles(loadedBackbone.articles)}
                             {/* <Route path="/shoppingcart" exact component={() => <Shoppingcart navs={navs} componentStructure={componentStructure} />} /> */}
                             {/* ToDo Error */}
