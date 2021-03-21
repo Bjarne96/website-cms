778e0b1d2960e4f36fdd03c3d31b1db86eeece0d
Bjarne96 <bjarne.abb@gmail.com>
2021-03-21 13:16:02 +0100

    produkt detail examples

--- src/app/views/produkte/produktdetail/produktdetail.tsx
+++ src/app/views/produkte/produktdetail/produktdetail.tsx
@@ -15,12 +15,29 @@ export class Produktdetail extends React.Component<IProps, {}>{
         super(props);
     }
 
+    test(src: string) {
+        console.log('test', src);
+        //@ts-ignore
+        //document.getElementById("test").src=src;
+    }
+
     render() {
         const produkt = this.props.produkt.variants[0];
+        console.log('this.props.produkt.properties', this.props.produkt.properties);
+        console.log('this.props.produkt.properties[0][1].name', this.props.produkt.properties[0][1].name);
+
         return <div>
             <div>{this.props.produkt.name}</div>
+            <div>{this.props.produkt.properties[0][0].name}</div>
+            <div>{this.props.produkt.properties[0][1].name}</div>
+            <div>{this.props.produkt.properties[0][2].name}</div>
+            <div>{this.props.produkt.properties[0][3].name}</div>
+            <div id="test" >{produkt.pictures[0]}</div>
+            <div>{produkt.pictures[1]}</div>
+            <div>{produkt.pictures[2]}</div>
+            {/* <div>{this.props.produkt.properties[1]}</div> */}
             <div>{produkt.price}</div>
-            <img src={produkt.pictures[0]} />
+            <img src={produkt.pictures[0]} onClick={() => this.test(produkt.pictures[0])} />
             <div className="product-description" dangerouslySetInnerHTML={{ __html: produkt.description }} />
         </div>
     }
